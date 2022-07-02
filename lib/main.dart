c78f520a5438d5d74f4fa08330ea5f066178c64b
eunsunglee <okskmk2@naver.com>
2022-07-02 15:11:47 +0900

    메인 파일 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:chang_mini/pages/splash_page.dart';
 import 'package:chang_mini/services/bank_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => BankService()),
+        ChangeNotifierProvider(create: (context) => BankService(prefs)),
       ],
       child: const MyApp(),
     ),
